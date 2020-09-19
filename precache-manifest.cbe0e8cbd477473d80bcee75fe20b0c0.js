8dfac1e94c178e6f37a89c9b6dbd8c0657e13f8e
ddwayne <652507565@qq.com>
2020-09-19 16:21:23 +0800

    deploy

---
+++ precache-manifest.cbe0e8cbd477473d80bcee75fe20b0c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0666edc3dd7bc0d53a",
+    "url": "/vue-ddmoney-website/css/app.96c20ff3.css"
+  },
+  {
+    "revision": "864ca43703431d7210eda30dd877b9b6",
+    "url": "/vue-ddmoney-website/index.html"
+  },
+  {
+    "revision": "0f0666edc3dd7bc0d53a",
+    "url": "/vue-ddmoney-website/js/app.af82a815.js"
+  },
+  {
+    "revision": "46613f8fb0d154ca63f0",
+    "url": "/vue-ddmoney-website/js/chunk-vendors.a7a265d4.js"
+  },
+  {
+    "revision": "2aaac70643daa8c5e0ea8f7b8413cc1c",
+    "url": "/vue-ddmoney-website/manifest.json"
+  },
+  {
+    "revision": "895bb2fd4e7cac9003059c1df431636f",
+    "url": "/vue-ddmoney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-ddmoney-website/robots.txt"
+  }
+]);
